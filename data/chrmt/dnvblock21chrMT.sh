d2a6c9f45f55e11bbea3b4998f6f989ec9d4a4e6
RuiDongDR <serenadong1993@outlook.com>
2021-09-03 11:45:15 +0800

    arrange files into folders

--- data/chrmt/dnvblock21chrMT.sh
+++
@@ -1,4 +0,0 @@
-for individual in NA21141 NA21142 NA21143 NA21144 
-do
- nohup perl NVdna_20190908.pl $individual.fa 4 > $individual.log 2>&1
-done
--- data/chrmt/dnvchmodchrMT.sh
+++
@@ -1,4 +0,0 @@
-for filename in dnvblock1chrMT.sh dnvblock2chrMT.sh dnvblock3chrMT.sh dnvblock4chrMT.sh dnvblock5chrMT.sh dnvblock6chrMT.sh dnvblock7chrMT.sh dnvblock8chrMT.sh dnvblock9chrMT.sh dnvblock10chrMT.sh dnvblock11chrMT.sh dnvblock12chrMT.sh dnvblock13chrMT.sh dnvblock14chrMT.sh dnvblock15chrMT.sh dnvblock16chrMT.sh dnvblock17chrMT.sh dnvblock18chrMT.sh dnvblock19chrMT.sh dnvblock20chrMT.sh dnvblock21chrMT.sh 
-do
- chmod +x ./$filename
-done
